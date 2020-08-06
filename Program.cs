0b102a3493479911b28dcd9a1fefbda4882132fd
Limon250 <45670406+Limon250@users.noreply.github.com>
2020-08-06 11:55:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Dynamic;
+using System.Diagnostics;
+using System;
+
+namespace Sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a value!");
+            double a = Double.Parse(Console.ReadLine());
+            Thread.Sleep(800);
+
+            double b = Math.Sqrt(a);
+            Thread.Sleep(800);
+            Console.WriteLine("Sqrt from {0} = {1}", a, b);
+
+            Thread.Sleep(800);
+            Console.WriteLine("To exit just press Enter");
+            Console.ReadKey();
+        }   
+    }
+}     
